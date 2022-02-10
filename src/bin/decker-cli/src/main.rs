720db6bdb397964fc7141d724b5554841bbf0e12
Luke Wolf <lukewolf101010@gmail.com>
2022-02-10 20:46:34 +0000

    Add an initial basic non-functional implementation to decker-cli

--- src/bin/decker-cli/src/main.rs
+++ src/bin/decker-cli/src/main.rs
@@ -1,3 +1,74 @@
+use std::path::PathBuf;
+
+use clap::{ArgEnum, Parser, Subcommand};
+
+#[derive(Parser)]
+#[clap(author,version,about,long_about = None)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Add {
+        #[clap(subcommand)]
+        command: AddCommands,
+    },
+    Remove {
+        #[clap(subcommand)]
+        command: SubCommands,
+    },
+    Enable {
+        #[clap(subcommand)]
+        command: SubCommands,
+    },
+    Disable {
+        #[clap(subcommand)]
+        command: SubCommands,
+    },
+}
+
+#[derive(Subcommand)]
+enum AddCommands {
+    Game {
+        /// The Name of the Game
+        #[clap(arg_enum)]
+        name: Games,
+        /// The Path to the Game
+        path: PathBuf,
+    },
+    Mod {
+        #[clap(arg_enum)]
+        game: Games,
+        game_mod: String,
+        path: PathBuf,
+    },
+}
+
+#[derive(Subcommand)]
+enum SubCommands {
+    Game {
+        /// The Name of the Game
+        #[clap(arg_enum)]
+        name: Games,
+    },
+    Mod {
+        /// The Name of the Game
+        #[clap(arg_enum)]
+        game: Games,
+        /// The Name of the Mod
+        game_mod: String,
+    },
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ArgEnum)]
+enum Games {
+    OpenMW,
+}
+
+#[derive(Subcommand)]
+enum ModCommands {}
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
 }
